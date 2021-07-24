2c765642b0cbcf626a204e883dc637edb667e315
Andy-Lopez <adnylopez@gmail.com>
2021-07-24 13:02:22 -0700

    stop main style

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
     eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("styles/main.css");
+    // eleventyConfig.addPassthroughCopy("styles/main.css");
 };
